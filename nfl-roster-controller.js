d2733fa691222d46f0f3aca8fbb832618568a325
joe <loseca@yahoo.com>
2016-04-23 14:54:55 -0600

    p

--- nfl-roster-controller.js
+++ nfl-roster-controller.js
@@ -1,59 +1,85 @@
 new PlayerService('http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json', ready)
 
 
-function ready(dataStore){
-  
-  var roster = $('#roster');
-  
-  var players = dataStore.getAllPlayers();
-  
-  $('button').on('click', function(event){   //populate function/ searching by whatever value
-      var query = $('#query').val()
-      players = dataStore.getPlayersBySomeValue(query)
-      update();
-  })
-  
-  $('.clear-filter-button').on('click', function(){  //reset
-      players = dataStore.getAllPlayers()
-      update();
-  })
-  
-  
-  function update(){
-    roster.empty()
-    roster.append('<h3>Showing '+players.length+' Active Players</h3>')// append glue to back of roster
+function ready(dataStore) {
 
-    players.forEach(function(player){
-        var template = '<div class="player-card"><img src="'+player.photo+'" width="80"/>'+player.fullname+'</div>'//populating the player card
-        roster.append(template);
-    })
+  var roster = $('#roster');
+
+  var players = dataStore.getAllPlayers();
+
+  var homeTeam = [];
+
+  //   $('#addPlayerBtn').on('click', function(event){   //populate function/ searching by whatever value
+
+  //   })
+
+  $('.clear-filter-button').on('click', function () {  //reset
+    $('#query').val('')
+    players = dataStore.getAllPlayers()
+    update();
+
+
+  })
+
+  $('form').on('submit', function (e) {
+    e.preventDefault()
+    var query = $('#query').val()
+    players = dataStore.getPlayersBySomeValue(query)
+    update();
+  })
+
+
+
+  function update() {
+    roster.empty()
+    roster.append('<h3>Showing ' + players.length + ' Active Players</h3>')// append glue to back of roster
+
+    players.forEach(function (player) {
+      var template = '<div class="player-card"><img src="' + player.photo + '" width="80"/>' + player.fullname + '<button id="' + player.fullname + '" class="btn">Draft Player</button></div>'    //populating the player card
       
+      roster.append(template);
+    })
+
   }
 
-update();  
-  
-}
-  
-     
-     
+  $("#roster").on("click", ".btn", function () {
+    var name = this.id
+    players.forEach(function (player){
 
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-   
+      if (player.fullname === name){
+        homeTeam.push(player);
+      }
+    })
+
+    players.splice(this.id, 1);
+
+
+  })
+
+
+  update();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- player-service.js
+++ player-service.js
@@ -1,7 +1,8 @@
 
 function PlayerService(url, callBack) {
-
+    var dataStore = this;
     var playerData = []
+   
 
     this.getAllPlayers = function () { // this is used so that this can be retrieved
         return playerData;
@@ -10,17 +11,17 @@ function PlayerService(url, callBack) {
     this.getPlayersBySomeValue = function (value) {   //BySome makes any identifier useable
 
         var team = [];
-        for (var i = 0; i < playerData.lenght; i++) {
+        for (var i = 0; i < playerData.length; i++) {
+
             var currentPlayer = playerData[i];
             var hasValue = false;
             for (var prop in currentPlayer) {
-                if (typeof currentPlayer[prop] === 'string' && currentPlayer[prop].toLowerCase() === value) { //string&& toLowerCase is strictly for the ability to pull out var cases
+                if (typeof currentPlayer[prop] === 'string' && currentPlayer[prop].toLowerCase() === value.toLowerCase()) { //string&& toLowerCase is strictly for the ability to pull out var cases
                     hasValue = true;
                 }
             }
             if (hasValue) {
                 team.push(currentPlayer)// gives use the info for team array above-builds team array
-
             }
         }
         return team;
@@ -28,6 +29,13 @@ function PlayerService(url, callBack) {
     }
 
     function goGetData() {
+
+        playerData = localStorage.getItem('playerData');
+        if (playerData) {
+            playerData = JSON.parse(playerData)
+            return callBack(dataStore);
+        }
+
         var BCWServer = "http://bcw-getter.herokuapp.com/?url=";
         var modifiedUrl = BCWServer + encodeURIComponent(url);
 
@@ -38,7 +46,8 @@ function PlayerService(url, callBack) {
                     return player;
                 }
             })
-            callBack(this)
+            localStorage.setItem('playerData', JSON.stringify(playerData))
+            callBack(dataStore)
         })
 
     }
